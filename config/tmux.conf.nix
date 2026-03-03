484023722af74bd025950c9e6eee1984433cd6e9
Noam Stolero <noam@factify.com>
2026-03-03 10:13:04 +0200

    feat: add btop floating popup keybinding (prefix+b)

--- config/tmux.conf.nix
+++ config/tmux.conf.nix
@@ -254,8 +254,9 @@
     bind-key "K" display-popup -E -w 40% -k "sesh connect \"$(sesh list -i | ${pkgs.gum}/bin/gum filter --no-strip-ansi --limit 1 --no-sort --fuzzy --placeholder 'Pick a sesh' --height 50 --prompt='⚡')\""
     bind-key "S" run-shell "sesh connect \"$(sesh list --icons | ${pkgs.fzf}/bin/fzf-tmux -p 90%,85% --no-sort --ansi --border-label '  Sessions ' --prompt '⚡  ' --header '  ^a all ^t tmux ^g configs ^x zoxide ^d kill ^f find' --bind 'tab:down,btab:up' --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' --bind 'ctrl-f:change-prompt(🔎  )+reload(${pkgs.fd}/bin/fd -H -d 2 -t d -E .Trash . ~)' --bind 'ctrl-d:execute(tmux kill-session -t {})' --preview-window 'right:60%:wrap' --preview 'sesh-preview {}')\""
 
-    # Lazygit floating popup
+    # Floating popups
     bind-key "g" display-popup -E -w 90% -h 90% lazygit
+    bind-key "b" display-popup -E -w 90% -h 90% btop
 
     # New session prompt
     bind N command-prompt -p "New session name:" "new-session -s '%%'"
